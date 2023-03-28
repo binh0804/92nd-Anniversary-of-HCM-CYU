44b9ad0ac47acf33cdf403b3b4af03a6dfeafdd1
knippink <20110252@student.hcmute.edu.vn>
2023-03-28 17:42:56 +0700

    init

---
+++ script/typer.js
@@ -0,0 +1,2 @@
+new TypeIt("#testt",{strings:'"Một năm khởi đầu từ mùa xuân. Một đời khởi đầu từ tuổi trẻ. Tuổi trẻ là mùa xuân của xã hội.<br>- Hồ Chí Minh"',speed:85,loop:!0}).go();
+// from DE.ts
